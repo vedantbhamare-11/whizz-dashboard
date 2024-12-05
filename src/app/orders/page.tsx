4f5a3141576b77dcaac36526735e2b85ef3dba2b
vedant-dev <vedantdbhamare@gmail.com>
2024-12-05 16:38:41 +0530

    added the orders page to whizz

---
+++ src/app/orders/page.tsx
@@ -0,0 +1,48 @@
+"use client";
+import { useState, useLayoutEffect } from "react";
+import Header from "@/components/layout/Header";
+import Sidebar from "@/components/layout/Sidebar";
+import OrderHistorySection from "@/components/orders/OrderHistorySection";
+
+const isMobile = () => window.innerWidth <= 820;
+
+const OrderHistory = () => {
+  const [isMobileView, setIsMobileView] = useState(false);
+
+  useLayoutEffect(() => {
+    const handleResize = () => setIsMobileView(isMobile());
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <div className="flex flex-col h-screen">
+      {isMobileView ? (
+        // Mobile Layout
+        <div className="flex flex-col h-screen w-full">
+          <Header />
+          <div className="flex flex-1 pt-20">
+            <Sidebar />
+            <main className="flex-1 p-4 mb-[5rem] flex flex-col space-y-4">
+              <OrderHistorySection />
+            </main>
+          </div>
+        </div>
+      ) : (
+        // Desktop Layout
+        <div className="flex h-full">
+          <Sidebar />
+          <div className="flex-1 flex flex-col">
+            <Header />
+            <main className="flex-1 p-4 mt-[3rem] ml-[5rem]">
+              <OrderHistorySection />
+            </main>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default OrderHistory;
